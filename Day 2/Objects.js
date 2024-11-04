30a64abef6013050fd2d83c5e542576aa51a729c
iamrohitsampannavar45 <imrohitsampannavar@gmail.com>
2024-11-04 14:54:10 +0530

    new content

--- Day 2/Objects.js
+++ Day 2/Objects.js
@@ -5,14 +5,14 @@
 
 
 
-var num = Math.random()
-console.log(num)
+// var num = Math.random()
+// console.log(num)
 
-var num1= Math.floor(44.66);
-console.log(num1);
+// var num1= Math.floor(44.66);
+// console.log(num1);
 
-var num2 = Math.PI
-console.log(num2);
+// var num2 = Math.PI
+// console.log(num2);
 
 
 //user Defined Objects
@@ -39,18 +39,52 @@ console.log(num2);
 
 
 
-var products = {
-    name: "Iphone 16 Pro",
-    price: 80000,
-    color:"Black",
-    manufacturer: "Apple Inc",
-    getModelDetails(){
-        return "This Phone is Manufactured By " +this.manufacturer+ "And the Name of the Mobile is "+ this.name
-        + "And the Price of the Iphone is "+ this.price +"and the color is " + this.color 
-    }
-}
+// var products = {
+//     name: "Iphone 16 Pro",
+//     price: 80000,
+//     color:"Black",
+//     manufacturer: "Apple Inc",
+//     getModelDetails(){
+//         return "This Phone is Manufactured By " +this.manufacturer+ "And the Name of the Mobile is "+ this.name
+//         + "And the Price of the Iphone is "+ this.price +"and the color is " + this.color 
+//     }
+// }
 
-console.log(products.name);
-console.log(products.price);
-console.log(products.manufacturer)
-console.log(products.getModelDetails());
+// console.log(products.name);
+// console.log(products.price);
+// console.log(products.manufacturer)
+// console.log(products.getModelDetails());
+
+
+//object Constructor 
+
+
+// var product  =  new Object();
+
+// product.name = "Iphone",
+// product.model = "15 Pro Max",
+// product.color = "Black",
+// product.price = 58522;
+
+// product.getProductDetails = function (){
+//     return `The Prouct is ${this.name} and price is ${this.price}`
+// }
+
+// console.log(product.getProductDetails())
+
+
+
+// var Profile = new Object();
+
+// Profile.name = "Rohit Sampannavar"
+// Profile.education = "BE CSE",
+// Profile.place = "Bengaluru",
+// Profile.dob = 2001
+
+// Profile.getDetails = function (){
+//     return  ` The Profile name is ${this.name} and completed graduation in ${this.education}`
+// }
+
+// console.log(Profile.name);
+// console.log(Profile.dob);
+// console.log(Profile.getDetails())
