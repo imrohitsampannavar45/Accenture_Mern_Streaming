0f636702a80e1947a3cd11c6c82fc32211b51dd8
iamrohitsampannavar45 <imrohitsampannavar@gmail.com>
2024-11-04 16:01:33 +0530

    new content

--- Day 2/Objects.js
+++ Day 2/Objects.js
@@ -88,3 +88,164 @@
 // console.log(Profile.name);
 // console.log(Profile.dob);
 // console.log(Profile.getDetails())
+
+
+
+
+//constructor function
+
+
+// function product(name, color, price) {
+//     this.pname = name;
+//     this.prodprice = price;
+//     this.pcolor = color;
+// this.getProductDetails = function(){
+//     return "My Mobile Phone is "+ this.pname;
+// }
+
+// }
+
+// var product1 = new product("Iphone", "Black", 12000)
+// var products = new product("Washing Machine ", "pink", 9000000);
+// console.log(product1.getProductDetails());
+// console.log(products.getProductDetails())
+
+
+
+
+// function resume(name, education, experience){
+//     this.name = name;
+//     this.education = education;
+//     this.experience = experience;
+//     this.getResume = function(){
+//         return "My Name is " + this.name + " "+ "Completed Education in" + 
+//          " " + this.education + " "+  " and having a expeience of " 
+//         + this.experience
+//     }
+// }
+
+// var demo = new resume("Rohit", "BCA", "2 Year of Experience");
+// console.log(demo.getResume())
+
+
+// //100 lines of code 
+
+// resume.prototype.getResumeDetails = function(){
+//     return "This is my Resume" + " " +this.name
+// }
+// console.log(demo.getResumeDetails())
+
+// resume.prototype.getExperience = function(){
+//     return "This candidate has this much of experience"+ " " + this.experience;
+
+// }
+// console.log(demo.getExperience())
+
+
+
+
+//prototype inheritance 
+
+// function Person(name,location,years){
+// this.name = name;
+// this.location = location;
+// this.years = years;
+
+
+// function getPersonDetails(){
+//     return "this person name is " + this.name
+// }
+// }
+
+// Person.prototype.welcome = function(){
+//     console.log("Deepika Welcome");
+// }
+
+
+
+// function vehicle (name, color, model){
+//     this.name = "maruti";
+//     this.color = "Blue";
+//     this.model = "New Model"
+// }
+
+// vehicle.prototype.speed = function(){
+//     console.log("This Vehicle Runs at High Speed");
+// }
+
+// vehicle.prototype.engine = function(){
+//     console.log(" this Vehicle Engine is Very Good");
+// }
+
+
+
+// function car(){
+
+//     this.car = new vehicle()
+    
+//     car.prototype.modelName = function(){
+//         console.log("The Car Model Name is Maruti Suzuki");
+//     }
+
+// }
+
+// console.log(car.engine())
+
+
+
+
+
+
+
+
+
+
+
+// function Student(){
+// Student.prototype = new Person()
+
+
+// Student.prototype.study = function (){
+// console.log(" I am Studying");s
+
+// }
+// }
+
+// var StudentInfo = new Student();
+// StudentInfo.study()
+// StudentInfo.welcome();
+
+
+
+
+
+function Vehical(){}
+
+Vehical.prototype.name = 'tata'
+Vehical.prototype.model = 'Nano'
+Vehical.prototype.details = function(){
+    console.log(`the name is ${this.name} and brand is ${this.model}`)
+}
+
+function Car(){}
+Car.prototype = new Vehical();
+
+Car.prototype.price = 250000;
+Car.prototype.details = function(){
+    console.log(`the price is ${this.price}`)
+}
+
+let CarObj = new car();
+CarObj.details();
+console.log(CarObj.name)
+CarObj.details()
+
+let VehicalObj = new Vehical()
+VehicalObj.details()
+
+//output
+//  node script.js
+// the name is vehical and brand is suzuki
+// suzuki
+// the price is 250000
+// the name is vehical and brand is suzuki
