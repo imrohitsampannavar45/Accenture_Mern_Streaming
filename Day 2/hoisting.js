e86f36e0aa61b8423b249600f3d82b79db5317c4
iamrohitsampannavar45 <imrohitsampannavar@gmail.com>
2024-11-04 11:21:05 +0530

    new file

---
+++ Day 2/hoisting.js
@@ -0,0 +1,43 @@
+// console.log(number)
+// Hello();
+// var number = 10;
+// console.log(product(1,3));
+// add();
+// function Hello(){
+//     console.log("hello");
+    
+// }
+
+// hoisting is possible for function statements
+// hoisting - is not supported for the - Function Expression
+
+// var product = function (a,b){
+//     return a*b;
+// }
+
+// const add= () =>{
+// console.log("ABCD");
+// }
+
+
+
+
+console.log(add(4,5))
+// console.log(mul(5,4));
+// app();
+console.log(app1(1,3));
+
+function add(a,b){
+    return a+b;
+}
+
+var mul = function multiplication(a,b){
+    return a*b;
+}
+
+const app=() =>{
+    console.log("App is Working ");
+}
+const app1=(a,b)=>{
+return a*b;
+}
