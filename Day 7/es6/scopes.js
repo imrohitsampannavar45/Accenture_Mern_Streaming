d5e1709698c96e1b06f966fb0fc50da946b6bed2
iamrohitsampannavar45 <imrohitsampannavar@gmail.com>
2024-11-11 12:58:26 +0530

    new changes

--- Day 7/es6/scopes.js
+++ Day 7/es6/scopes.js
@@ -1,1 +1,29 @@
-let a = 10;
+// before  we had 
+
+
+
+function display () {
+
+var msg = "Good Morning"
+
+if(true){
+    var msg = "Hello"
+    console.log(msg);
+}
+
+console.log("outside block ----------")
+console.log(msg);
+
+}
+
+
+display()
+
+
+
+
+
+
+// var = function scope - redeclare - reassign 
+// let = block scope - no redeclare - reassign
+// const = block scope -no redeclare - no reassign 
