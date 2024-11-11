8c87f84ce7f883ed6d314e0674515d559601c5c1
iamrohitsampannavar45 <imrohitsampannavar@gmail.com>
2024-11-11 12:37:46 +0530

    new changes

---
+++ Day 7/es6/sample.js
@@ -0,0 +1,33 @@
+let a=10
+console.log(typeof a)
+a=20
+console.log(typeof a)
+console.log(a+10)
+ 
+let msg="hello"
+console.log(typeof msg)
+let islogin=true
+console.log(typeof islogin)
+ 
+let x
+console.log(typeof x)
+//x=10
+//console.log(typeof x)
+ 
+let y=null
+console.log(typeof y)
+ 
+console.log("x==y:"+(x==y))//will check for values
+console.log("x===y:"+(x===y))//will check for values and datatype
+ 
+ 
+let nums=[10,20,30]
+console.log(typeof nums)
+ 
+let product={
+    name:"mobile",
+    brand:"dell",
+    price:10000
+}
+ 
+console.log(typeof product)
