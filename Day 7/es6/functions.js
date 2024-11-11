05ada83bc4c38c3b0930bc82344963703ad062f6
iamrohitsampannavar45 <imrohitsampannavar@gmail.com>
2024-11-11 14:32:06 +0530

    functions

---
+++ Day 7/es6/functions.js
@@ -0,0 +1,55 @@
+// function sayHello() {
+//     console.log("Hello");
+// }
+
+// sayHello()    =>  Function Statement
+
+
+
+// function echo(msg)
+// {
+//     console.log(msg)
+// }
+
+// echo("hello")
+// echo("hi")
+
+
+// function add(n1,n2){
+//     return n1+n2 // 10 + undefined => NaN
+//                 //  undefined + undefined = > NaN   
+// }
+
+// console.log(add(10,20));
+// console.log(add(10,"20"))
+// console.log(add(10))
+// console.log(add())
+
+
+
+//function expression
+
+let sub = function(n1,n2){
+    return n1-n2
+}
+
+
+let mul = function(n1,n2){
+    return n1*n2
+}
+
+console.log(typeof sub)
+
+console.log(sub(10,20))
+
+
+function calculate(num1, num2,opertor) // higher order function
+ {
+   console.log( opertor(num1,num2))
+
+}
+
+calculate(10,20,sub) // sub is a callback
+calculate(10,20,mul) // mul is a callback
+calculate(10,20,function(n1,n2){return n1* n2})
+calculate(10,20,function(n1,n2){return n1 / n2})
