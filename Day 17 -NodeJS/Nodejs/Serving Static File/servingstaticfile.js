b5b33a462aba1ac136a853196f9ea5db9cfde818
iamrohitsampannavar45 <imrohitsampannavar@gmail.com>
2024-11-27 16:08:56 +0530

    new changes

--- Day 17 -NodeJS/Nodejs/Serving Static File/servingstaticfile.js
+++ Day 17 -NodeJS/Nodejs/Serving Static File/servingstaticfile.js
@@ -1,9 +1,15 @@
 let express = require('express')
 let appserver = express()
 
-appserver.use("/terms", express.static("public"))
-// appserver.use("/index", express.static("public"))
+let PORT = 3000;
+
+// appserver.use("/terms", express.static("public"))
+appserver.use("/home", express.static("public"))
+
+
+
+
 //http:localhost:3000/terms/terms&conditions.txt
 
 
-appserver.listen(3000,()=> console.log("server is ready to serve the static file "))
+appserver.listen(PORT,()=> console.log(`server is ready to serve the static file at port no ${PORT}   `))
