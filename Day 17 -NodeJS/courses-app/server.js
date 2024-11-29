55aff7ea16ae652b762ba9d482b260d5e2806c61
iamrohitsampannavar45 <imrohitsampannavar@gmail.com>
2024-11-29 09:39:04 +0530

    new changes in the server.js file

--- Day 17 -NodeJS/courses-app/server.js
+++ Day 17 -NodeJS/courses-app/server.js
@@ -5,11 +5,9 @@ const cors = require('cors');
 const app = express();
 const PORT = 5000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// MongoDB connection
 mongoose.connect('mongodb://localhost:27017/ShiftLeft1', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -21,16 +19,18 @@ db.once('open', () => {
     console.log('Connected to MongoDB');
 });
 
-// Course Schema
+
 const courseSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    duration: Number,
+    cname: String,
+    ccode: Number,
+    cfees: Number,
+    sname: String,
+
+
 });
 
 const Course = mongoose.model('Course', courseSchema, '2.Courses');
 
-// Routes
 app.get('/courses', async (req, res) => {
     try {
         const courses = await Course.find();
