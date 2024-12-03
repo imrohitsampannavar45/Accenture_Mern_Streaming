c06ce119f19e03dcd58ea1311713428cc82aebb9
iamrohitsampannavar45 <imrohitsampannavar@gmail.com>
2024-12-03 15:41:22 +0530

    changes in the npm publish

---
+++ NODEJS CONCEPTS/mynpmmodule/index.js
@@ -0,0 +1,4 @@
+let echo = function () {
+    console.log("Hi Welcome, HI-FI");
+}
+module.exports = {echo};
---
+++ NODEJS CONCEPTS/testnpmModule/server.js
@@ -0,0 +1,3 @@
+const echomyname = require('rohitsampannavarfirstmodule')
+
+echomyname.echo()
